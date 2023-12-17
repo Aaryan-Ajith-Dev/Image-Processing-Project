c5ac583be47b606670f1c07d25b744ccb5cc9fd3
Kandarp08 <Dave.Kandarp@iiitb.ac.in>
2023-12-17 18:17:48 +0530

    Finished dominant colour

--- ImageEffectBackend/src/Libraries/DominantColourLibrary/DominantColour.cpp
+++ ImageEffectBackend/src/Libraries/DominantColourLibrary/DominantColour.cpp
@@ -26,9 +26,12 @@ void dominantColourEffect(vector<vector<Pixel>>& imageVector)
 
     for (i = 0; i < k; ++i)
     {
-        centroids[i].push_back(rand() % 256 / (i + 1));
-        centroids[i].push_back(rand() % 256 / (i + 1));
-        centroids[i].push_back(rand() % 256 / (i + 1));
+        int x = rand() % n;
+        int y = rand() % m;
+
+        centroids[i].push_back(imageVector[x][y].r);
+        centroids[i].push_back(imageVector[x][y].g);
+        centroids[i].push_back(imageVector[x][y].b);
     }
 
     int clusterNumber[n][m];
@@ -55,58 +58,33 @@ void dominantColourEffect(vector<vector<Pixel>>& imageVector)
         }
     }
 
-    for (int iter = 1; iter <= 20; ++iter)
+    double sum[k][3];
+    int freq[k] = {0};
+
+    for (i = 0; i < k; ++i)
+        sum[i][0] = sum[i][1] = sum[i][2] = 0;
+
+    for (i = 0; i < n; ++i)
     {
-        double sum[k][3];
-        int freq[k] = {0};
-
-        for (i = 0; i < k; ++i)
-            sum[i][0] = sum[i][1] = sum[i][2] = 0;
-
-        for (i = 0; i < n; ++i)
+        for (j = 0; j < m; ++j)
         {
-            for (j = 0; j < m; ++j)
-            {
-                int cluster = clusterNumber[i][j];
-                sum[cluster][0] += imageVector[i][j].r;
-                sum[cluster][1] += imageVector[i][j].g;
-                sum[cluster][2] += imageVector[i][j].b;
-                ++freq[cluster];
-            }
+            int cluster = clusterNumber[i][j];
+            sum[cluster][0] += imageVector[i][j].r;
+            sum[cluster][1] += imageVector[i][j].g;
+            sum[cluster][2] += imageVector[i][j].b;
+            ++freq[cluster];
         }
+    }
 
-        for (i = 0; i < k; ++i)
-        {
-            double r = sum[i][0] / freq[i];
-            double g = sum[i][1] / freq[i];
-            double b = sum[i][2] / freq[i];
+    for (i = 0; i < k; ++i)
+    {
+        double r = sum[i][0] / freq[i];
+        double g = sum[i][1] / freq[i];
+        double b = sum[i][2] / freq[i];
 
-            centroids[i][0] = r;
-            centroids[i][1] = g;
-            centroids[i][2] = b;
-        }
-
-        for (i = 0; i < n; ++i)
-        {
-            for (j = 0; j < m; ++j)
-            {
-                int cluster = 0;
-                double minDistance = calculateDistance(centroids[0], imageVector[i][j]);
-
-                for (int ii = 1; ii < k; ++ii)
-                {
-                    double distance = calculateDistance(centroids[ii], imageVector[i][j]);
-
-                    if (distance < minDistance)
-                    {
-                        cluster = ii;
-                        minDistance = distance;
-                    }
-                }
-
-                clusterNumber[i][j] = cluster;
-            }
-        }
+        centroids[i][0] = r;
+        centroids[i][1] = g;
+        centroids[i][2] = b;
     }
 
     for (i = 0; i < n; ++i)
