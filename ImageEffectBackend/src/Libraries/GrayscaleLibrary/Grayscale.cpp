dc057f633252a15d3ee5f51eb06cdd07e223b0d0
Prateek-Rath <122281363+Prateek-Rath@users.noreply.github.com>
2023-12-26 14:25:36 +0530

    commented Grayscale.cpp

--- ImageEffectBackend/src/Libraries/GrayscaleLibrary/Grayscale.cpp
+++ ImageEffectBackend/src/Libraries/GrayscaleLibrary/Grayscale.cpp
@@ -1,15 +1,12 @@
 #include "Grayscale.h"
 #include<bits/stdc++.h>
 using namespace std;
-vector<vector<Pixel>> grayscaleconvert(vector<vector<Pixel>> &v){//just for safety if reference doesn't work, we will return the pixels
-    //we have a reference to that vector, so whatever we do will effect that vector only
+vector<vector<Pixel>> grayscaleconvert(vector<vector<Pixel>> &v){//function returns a vector of vectors
     for(int i=0; i<v.size(); i++){
         for(int j=0; j<v[i].size(); j++){
             Pixel p = v[i][j];
-            double average = (p.b + p.g + p.r)/3.0;
-            // p.b = p.g = p.r = (int)average;
-            // p.b = p.g = p.r = 255;
-            v[i][j].r = v[i][j].b = v[i][j].g= (int)average;
+            double average = (p.b + p.g + p.r)/3.0; // grayscale is the average of r, g, b components for each pixel
+            v[i][j].r = v[i][j].b = v[i][j].g= (int)average;// type cast and assign
         }
     }
     return v;
